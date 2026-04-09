7f557ab36e8d24e9b6ebfbb0402608900f152994
Emi <e.lorenzo0203@gmail.com>
2026-04-09 13:14:58 +0200

    Working on BloomFilter class. Hashes have been implemented but maybe more optimized functions are available

--- BloomFilter.py
+++ BloomFilter.py
@@ -5,6 +5,9 @@ class BloomFilter:
     def __init__(self, expected_elements:int, false_positive_rate:float):
         self.n = expected_elements
         self.p = false_positive_rate
+        self.m, self.k = self._calculate_params()
+        self.bitmap = bitarray(self.m)
+        self.bitmap.setall(0) #to set all 0
 
     def _calculate_params(self)-> tuple[int, int]:
         if self.n <= 0:
@@ -24,4 +27,34 @@ class BloomFilter:
         k = max(1, k)
 
         return m, k
+
+    def _to_bytes(self, item) -> bytes:
+        # If it is already bytes, do nothing
+        if isinstance(item, bytes):
+            return item
+
+        # If it's a string, skip str() and go straight to encode
+        if isinstance(item, str):
+            return item.encode('utf-8')
+
+        # If it's an integer, convert it directly to bytes without going through the string
+        if isinstance(item, int):
+            return item.to_bytes(8, 'big', signed=True)
+
+        # Fallback for other types (less frequent)
+        return str(item).encode('utf-8')
+
+    def _get_hashes(self, item):
+        """Generate k indices within the range [0, m-1] for a given element."""
+        # converting the item to string and then to byte for hashing
+        item_bytes = self._to_bytes(item)
+
+        # We generate two basic hashes (SHA-256 broken in half)
+        digest = hashlib.sha256(item_bytes).digest()
+        h1 = int.from_bytes(digest[:4], 'big')
+        h2 = int.from_bytes(digest[4:8], 'big')
+
+        for i in range(self.k):
+            # Double hashing formula to obtain k different indices
+            yield (h1 + i * h2) % self.m
 
