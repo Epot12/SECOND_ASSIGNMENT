746ac5c048c3cedc0be7bafe751747b43c0bfdd8
Epot12 <e.lorenzo0203@gmail.com>
2026-04-09 11:24:32 +0200

    Working on the sequential version of Bloom Filter

---
+++ BloomFilter.py
@@ -0,0 +1,26 @@
+import math
+
+class BloomFilter:
+    def __init__(self, expected_elements:int, false_positive_rate:float):
+        self.n = expected_elements
+        self.p = false_positive_rate
+
+    def calculate_params(self)-> tuple[int, int]:
+        if self.n <= 0:
+            raise ValueError(f"The number of expected items (n) must be > 0. Received: {self.n}")
+        if not (0.0 < self.p < 1.0):
+            raise ValueError(f"The probability of false positives (p) must be between 0 and 1. Received: {self.p}")
+
+        #Calculation of m: Size of the bit array
+        m_float = -(self.n * math.log(self.p)) / (math.log(2) ** 2)
+        m = math.ceil(m_float)
+
+        #Calculation of k: Number of hash functions
+        k_float = (m / self.n) * math.log(2)
+        k = round(k_float)
+
+        # Sanity check
+        k = max(1, k)
+
+        return m, k
+
