17ae9031315e72838447d71c78a655fe8a9e00e1
Emi <e.lorenzo0203@gmail.com>
2026-04-13 20:45:57 +0200

    Backup commit

---
+++ BENCHMARKS/bench_amdahl.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import json
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+# importing scaling engine and filters
+from utils.scaling_engine import run_strong_scaling
+# Sostituisci questi import con i nomi corretti delle tue classi migliori
+from MULTITHREAD.MapReduceScalBloom import ThreadedScalableBloomFilter as MapReduceNoGIL
+from PARALLEL.PermPoolBloom import PermPoolScalableBloomFilter as SotaIPC
+
+
+def run():
+    print("\n[AMDAHL WORKER] Starting Strong Scaling Benchmarks...")
+
+    max_cores = 8  # Fisici + Logici
+    fixed_size = 3_000_000
+
+    results = {}
+
+    # 1. Test No-GIL
+    print("Testing No-GIL Architecture...")
+    results["Amdahl_NoGIL"] = run_strong_scaling(MapReduceNoGIL, max_cores, fixed_size)
+
+    # 2. Test IPC Multiprocessing
+    print("Testing IPC Architecture...")
+    results["Amdahl_IPC"] = run_strong_scaling(SotaIPC, max_cores, fixed_size)
+
+    # Salva il file JSON per il Grand Master
+    out_path = os.path.join(current_dir, 'telemetry_amdahl.json')
+    with open(out_path, 'w') as f:
+        json.dump(results, f, indent=4)
+
+    print(f"[AMDAHL WORKER] Telemetry saved to {out_path}")
+
+
+if __name__ == "__main__":
+    run()
